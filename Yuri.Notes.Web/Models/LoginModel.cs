b74b65dd30e00fc4804dd79c3d763eeeabc45263
GeorgeSaturday <subbotin.97@inbox.ru>
2018-11-10 20:24:13 +0300

    1

---
+++ Yuri.Notes.Web/Models/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Yuri.Notes.Web.Model
+{
+    public class LoginModel
+    {
+        [Required]
+        public string Login { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
